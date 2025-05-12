d676e9fe25ddce79b861a127457e57531bc5ad75
Vlad <iancuvladalexandru@gmail.com>
2025-05-12 19:08:45 +0300

    Initial Commit

---
+++ src/test/java/com/example/price_comparator/PriceComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.price_comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
